591c5fd854e08727e19a906afb41118e3ccf5190
Henrique Souza <henrique.r.s.hr@gmail.com>
2023-08-30 00:36:53 -0300

    feito mais perfeito

--- Program.cs
+++ Program.cs
@@ -1,37 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("teste");
+﻿public class Teste
+{
+    static void Main(string[] args)
+    {
+        Usuario usuario = new Usuario
+        {
+            Nome = "Joao",
+            Email = "joapedro@test.com",
+            SenhaEncriptografada = "senhasegura",
+            TipoDeUsuario = (UsuarioTipoEnum)0,
+        };
 
-var teste1 = (UsuarioTipoEnum)0;
-Console.WriteLine(teste1);
+        Categoria categoria = new Categoria { Id = 1, Descricao = "Direito" };
+        Idioma idioma = new Idioma { Id = 1, Descricao = "Portugues - Brasileiro" };
+        Pais pais = new Pais { Id = 1, Descricao = "Brasil" };
+
+
+        Produto produto = new Produto
+        {   
+            UsuarioCriador = usuario,
+            Nome = "Produto1",
+            Descricao = "Descricao",
+            CategoriaProduto = categoria, 
+            IdiomaDoProduto = idioma , 
+            PrincipalPais = pais ,
+        };
+
+        System.Console.WriteLine(produto.Id);
+        System.Console.WriteLine(produto.Nome);
+        System.Console.WriteLine(produto.UsuarioCriador?.Nome);
+        System.Console.WriteLine(produto.CategoriaProduto?.Descricao);
+        System.Console.WriteLine(produto.PrincipalPais?.Descricao);
+        System.Console.WriteLine(produto.IdiomaDoProduto?.Descricao);
+        System.Console.WriteLine(usuario.Id);
+        System.Console.WriteLine(usuario.TipoDeUsuario);
+    }
+}
 
 public class Usuario
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string NomeCompletoUsuario { get; set; } = string.Empty;
-    public string EmailUsuario { get; set; } = string.Empty;
-    public string SenhaCriptografada { get; set; } = string.Empty;
-    public UsuarioTipoEnum? TipoDeUsuario { get; set; } = null;
-    public DateTime CriacaoDataHora { get; set; } = DateTime.UtcNow;
-    public DateTime DataHoraAlteracao { get; set; } = DateTime.UtcNow;
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string SenhaEncriptografada { get; set; } = string.Empty;
+    public UsuarioTipoEnum? TipoDeUsuario { get; set; }
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
 }
 
 public enum UsuarioTipoEnum
 {
-    Vendedor,
     Comprador,
+    Vendedor,
 }
-
 
 public class Produto
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public Usuario? UsuarioCriador { get; set; }
+    public Usuario? UsuarioCriador { get; set; } = null;
     public string Nome { get; set; } = string.Empty;
     public string Descricao { get; set; } = string.Empty;
-    public Idioma? IdiomaProduto { get; set; } = null;
-    public Pais? PaisPrincipal { get; set; } = null;
+    public Idioma? IdiomaDoProduto { get; set; }
+    public Pais? PrincipalPais { get; set; }
     public string ImagemProdutoUrl { get; set; } = string.Empty;
-    public Categoria? CategoriaProduto { get; set; } = null;
+    public Categoria? CategoriaProduto { get; set; }
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
 
 }
 
@@ -39,26 +72,24 @@ public class Idioma
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
 }
 
 public class Pais
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
 }
 
 public class Categoria
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
 }
+
 
