394228bdf418d11a907338d3c801e0d2cc097704
Henrique Souza <henrique.r.s.hr@gmail.com>
2023-08-31 00:19:52 -0300

    treinando

--- Program.cs
+++ Program.cs
@@ -1,46 +1,60 @@
-﻿public class Teste
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+public class Teste
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         Usuario usuario = new Usuario
         {
-            Nome = "Pedro",
-            Email = "pedrofraga@test.com",
-            SenhaCriptografada = "senhasegura",
-            TipoDoCliente = (TipoDoClienteEnum)0,
+            Nome = "Roberto Carlos",
+            Email = "robertoc@gmail.com",
+            SenhaCriptograda = "ladylaura2512",
+            TipoDoUsuario = 0,
         };
 
-        Idioma idioma = new Idioma { Id = 1, Descricao = "Portugues - BR" };
-        Pais pais = new Pais { Id = 1, Descricao = "Brasil" };
-        Categoria categoria = new Categoria { Id = 1, Descricao = "Direito" };
+        Idioma idioma = new Idioma
+        {
+            Id = 1,
+            Descricao = "Portugues - BR",
+        };
+
+        Pais pais = new Pais
+        {
+            Id = 1,
+            Descricao = "Brasil",
+        };
+
+        Categoria categoria = new Categoria
+        {
+            Id = 1,
+            Descricao = "Musica",
+        };
 
         Produto produto = new Produto
         {
-            UsuarioCriadorDoProduto = usuario,
-            NomeProduto = "Loren",
-            DescricaoProduto = "Ipsum",
+            NomeProduto = "Aulas de canto com o Rei",
+            DescricaoProduto = "Aulas de canto ministrada pelo grande rei Roberto Carlos",
             IdiomaProduto = idioma,
             PaisProduto = pais,
-            ImagemProdutoUrl = "https://chat.openai.com/c/4733d5cf-2ded-45d9-a13f-4959245c7b90",
+            ImagemProdutoUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Frobertocarlos&psig=AOvVaw2eAwirO1utbh6fnzdA7ntj&ust=1693538102909000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjG8Jn3hYEDFQAAAAAdAAAAABAE",
             CategoriaProduto = categoria,
         };
 
-        //Usuario
-        System.Console.WriteLine(usuario.Id);
+        //Cliente
         System.Console.WriteLine(usuario.Nome);
         System.Console.WriteLine(usuario.Email);
-        System.Console.WriteLine(usuario.SenhaCriptografada);
-        System.Console.WriteLine(usuario.TipoDoCliente);
+        System.Console.WriteLine(usuario.SenhaCriptograda);
+        System.Console.WriteLine(usuario.TipoDoUsuario);
 
         //Produto
-        System.Console.WriteLine(produto.Id);
-        System.Console.WriteLine(produto.UsuarioCriadorDoProduto?.Nome);
         System.Console.WriteLine(produto.NomeProduto);
         System.Console.WriteLine(produto.DescricaoProduto);
         System.Console.WriteLine(produto.IdiomaProduto.Descricao);
         System.Console.WriteLine(produto.PaisProduto.Descricao);
         System.Console.WriteLine(produto.ImagemProdutoUrl);
         System.Console.WriteLine(produto.CategoriaProduto.Descricao);
+        System.Console.WriteLine(produto.DataHoraCriacao);
     }
 }
 
@@ -49,13 +63,13 @@ public class Usuario
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Nome { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string SenhaCriptografada { get; set; } = string.Empty;
-    public TipoDoClienteEnum? TipoDoCliente { get; set; }
-    public DateTime? HoraDataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime? HoraDataAlteracao { get; set; } = DateTime.UtcNow;
+    public string SenhaCriptograda { get; set; } = string.Empty;
+    public TipoDeUsuarioEnum? TipoDoUsuario { get; set; }
+    public DateTime? DataHoraCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataHoraAlteracao { get; set; }
 }
 
-public enum TipoDoClienteEnum
+public enum TipoDeUsuarioEnum
 {
     Vendedor,
     Comprador
@@ -64,37 +78,40 @@ public enum TipoDoClienteEnum
 public class Produto
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public Usuario? UsuarioCriadorDoProduto { get; set; } = null;
     public string NomeProduto { get; set; } = string.Empty;
     public string DescricaoProduto { get; set; } = string.Empty;
     public Idioma? IdiomaProduto { get; set; }
     public Pais? PaisProduto { get; set; }
     public string ImagemProdutoUrl { get; set; } = string.Empty;
     public Categoria? CategoriaProduto { get; set; }
-    public DateTime? HoraDataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime? HoraDataAlteracao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataHoraCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataHoraAlteracao { get; set; }
+
 }
 
 public class Idioma
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime? HoraDataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime? HoraDataAlteracao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
+
 }
 
 public class Pais
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime? HoraDataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime? HoraDataAlteracao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
+
 }
 
 public class Categoria
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime? HoraDataCriacao { get; set; } = DateTime.UtcNow;
-    public DateTime? HoraDataAlteracao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataHoraCriacao { get; set; }
+    public DateTime? DataHoraAlteracao { get; set; }
+
 }
