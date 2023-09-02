3664a91813e80d03aac81dfa8fc926e932ee6e16
Henrique Souza <henrique.r.s.hr@gmail.com>
2023-09-01 22:57:32 -0300

    ultima chance

--- Program.cs
+++ Program.cs
@@ -2,97 +2,85 @@
 {
     static void Main(string[] args)
     {
-        Usuario usuario = new Usuario {
-            Nome = "Roberto Carlos" , 
-            Email = "reirobertocarlos@gmail.com", 
-            SenhaEncriptografada = "ladylaura123", 
-            TipoDeUsuario = (TipoDeUsuarioEnum)0,              
+        Usuario usuario = new Usuario
+        {
+            Nome = "Pedro Alvez",
+            Email = "alvezpedro1998@gmail.com",
+            SenhaCriptografada = "avpedro1442",
+            TipoDoUsuario = (TipoDeUsuarioEnum)1
         };
 
-        Idioma idioma = new Idioma {
-            Id = 1,
-            Descricao ="Portugues",
+        Idioma idioma = new Idioma { Id = 2, Descricao = "Portugues-Br" };
+        Pais pais = new Pais { Id = 1, Descricao = "Brasil" };
+        Categoria categoria = new Categoria { Id = 12, Descricao = "Musica" };
+
+        Produto produto = new Produto
+        {
+            Nome = "Aula de canto",
+            Descricao = "Aulas de canto para inciantes",
+            UsuarioCriador = usuario,
+            IdiomaDoProduto = idioma,
+            PaisDoProduto = pais,
+            ImagemDoProdutoUrl = "Url da imagem",
+            CategoriaDoProduto = categoria
         };
 
-        Pais pais = new Pais {
-            Id = 1,
-            Descricao = "Brasil",
-        };
-
-        Categoria categoria = new Categoria{
-            Id = 22, 
-            Descricao = "Musica"
-         };
-
-         Produto produto = new Produto { 
-            IdCriador = usuario,
-            NomeDoProduto = "Aulas de canto com o Rei Roberto Carlos",
-            DescricaoDoProduto = "Roberto Carlos ensina a voce criar e cantar suas musicas", 
-            IdiomaProduto = idioma, 
-            PaisPrincipal = pais, 
-            ImagemProdutoUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FRobertoCarlosOficial%2F&psig=AOvVaw3Sh-yQf3T_q6XXiPsB5SSC&ust=1693602385993000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCPjcn9bmh4EDFQAAAAAdAAAAABAE",
-            CategoriaProduto = categoria,            
-          };
-
-        //Usuario
+        System.Console.WriteLine("-----Dados do usuario-----");
         System.Console.WriteLine(usuario.Id);
-          System.Console.WriteLine(usuario.Nome);
-          System.Console.WriteLine(usuario.Email);
-          System.Console.WriteLine(usuario.SenhaEncriptografada);
-          System.Console.WriteLine(usuario.TipoDeUsuario);
-          System.Console.WriteLine(usuario.CriacaoDataHora);
+        System.Console.WriteLine(usuario.Nome);
+        System.Console.WriteLine(usuario.Email);
+        System.Console.WriteLine(usuario.SenhaCriptografada);
+        System.Console.WriteLine(usuario.TipoDoUsuario);
+        System.Console.WriteLine(usuario.CriacaoDataHora);
 
-        //produto
+        System.Console.WriteLine("-----Dados do produto-----");
         System.Console.WriteLine(produto.Id);
-        System.Console.WriteLine(produto.IdCriador.Id);
-        System.Console.WriteLine(produto.NomeDoProduto);
-        System.Console.WriteLine(produto.DescricaoDoProduto);
-        System.Console.WriteLine(produto.IdiomaProduto.Descricao);
-        System.Console.WriteLine(produto.PaisPrincipal.Descricao);
-        System.Console.WriteLine(produto.ImagemProdutoUrl);
-        System.Console.WriteLine(produto.CategoriaProduto.Descricao);
-        System.Console.WriteLine(produto.CriacaoDataHora);
+        System.Console.WriteLine(produto.UsuarioCriador.Id);
+        System.Console.WriteLine(produto.Nome);
+        System.Console.WriteLine(produto.Descricao);
+        System.Console.WriteLine(produto.IdiomaDoProduto.Descricao);
+        System.Console.WriteLine(produto.PaisDoProduto.Descricao);
+        System.Console.WriteLine(produto.ImagemDoProdutoUrl);
+        System.Console.WriteLine(produto.CategoriaDoProduto.Descricao);
     }
 }
 
 public class Usuario
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; init; } = Guid.NewGuid();
     public string Nome { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string SenhaEncriptografada { get; set; } = string.Empty;
-    public TipoDeUsuarioEnum? TipoDeUsuario { get; set; } = null;
-    public DateTime? CriacaoDataHora { get; set; } = DateTime.UtcNow;
+    public string SenhaCriptografada { get; set; } = string.Empty;
+    public TipoDeUsuarioEnum? TipoDoUsuario { get; set; } = null;
+    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
     public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-
 }
 
 public enum TipoDeUsuarioEnum
 {
     vendedor,
-    comprador,
+    comprador
 }
 
 public class Produto
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public Usuario? IdCriador { get; set; } = null;
-    public string NomeDoProduto { get; set; } = string.Empty;
-    public string DescricaoDoProduto { get; set; } = string.Empty;
-    public Idioma? IdiomaProduto { get; set; } = null;
-    public Pais? PaisPrincipal { get; set; } = null;
-    public string ImagemProdutoUrl { get; set; } = string.Empty;
-    public Categoria? CategoriaProduto { get; set; } = null;
-    public DateTime? CriacaoDataHora { get; set; } = DateTime.UtcNow;
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public Usuario? UsuarioCriador { get; init; } = null;
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public Idioma? IdiomaDoProduto { get; set; } = null;
+    public Pais? PaisDoProduto { get; set; } = null;
+    public string ImagemDoProdutoUrl { get; set; } = string.Empty;
+    public Categoria? CategoriaDoProduto { get; set; } = null;
+    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
     public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-
 }
 
 public class Idioma
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime? CriacaoDataHora { get; set; } = DateTime.UtcNow;
+    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
     public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
 }
 
@@ -100,7 +88,7 @@ public class Pais
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime? CriacaoDataHora { get; set; } = DateTime.UtcNow;
+    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
     public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
 }
 
@@ -108,6 +96,6 @@ public class Categoria
 {
     public int Id { get; set; }
     public string Descricao { get; set; } = string.Empty;
-    public DateTime? CriacaoDataHora { get; set; } = DateTime.UtcNow;
+    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
     public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
 }
