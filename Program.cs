3fedcf8b829d38b8deebf4f9fcadd9789cddd7eb
Iaomist-notserviço <77891303+Iaomist@users.noreply.github.com>
2023-09-04 17:31:13 -0300

    treinamento palindromo

--- Program.cs
+++ Program.cs
@@ -1,101 +1,30 @@
-﻿public class Teste
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+public class Programa
 {
+    public static int Palindromo(int Numero)
+    {
+        string NumeroString = Numero.ToString();
+        char[] charArray = NumeroString.ToCharArray();
+        Array.Reverse(charArray);
+        string reverseArray = new string(charArray);
+
+        if (nuero == reverseArray)
+        {
+            System.Console.WriteLine(" verdade");
+        }
+        else
+        {
+            System.Console.WriteLine(" falso");
+        }
+
+        return Numero;
+    }
     static void Main(string[] args)
     {
-        Usuario usuario = new Usuario
-        {
-            Nome = "Pedro Alvez",
-            Email = "alvezpedro1998@gmail.com",
-            SenhaCriptografada = "avpedro1442",
-            TipoDoUsuario = (TipoDeUsuarioEnum)1
-        };
-
-        Idioma idioma = new Idioma { Id = 2, Descricao = "Portugues-Br" };
-        Pais pais = new Pais { Id = 1, Descricao = "Brasil" };
-        Categoria categoria = new Categoria { Id = 12, Descricao = "Musica" };
-
-        Produto produto = new Produto
-        {
-            Nome = "Aula de canto",
-            Descricao = "Aulas de canto para inciantes",
-            UsuarioCriador = usuario,
-            IdiomaDoProduto = idioma,
-            PaisDoProduto = pais,
-            ImagemDoProdutoUrl = "Url da imagem",
-            CategoriaDoProduto = categoria
-        };
-
-        System.Console.WriteLine("-----Dados do usuario-----");
-        System.Console.WriteLine(usuario.Id);
-        System.Console.WriteLine(usuario.Nome);
-        System.Console.WriteLine(usuario.Email);
-        System.Console.WriteLine(usuario.SenhaCriptografada);
-        System.Console.WriteLine(usuario.TipoDoUsuario);
-        System.Console.WriteLine(usuario.CriacaoDataHora);
-
-        System.Console.WriteLine("-----Dados do produto-----");
-        System.Console.WriteLine(produto.Id);
-        System.Console.WriteLine(produto.UsuarioCriador.Id);
-        System.Console.WriteLine(produto.Nome);
-        System.Console.WriteLine(produto.Descricao);
-        System.Console.WriteLine(produto.IdiomaDoProduto.Descricao);
-        System.Console.WriteLine(produto.PaisDoProduto.Descricao);
-        System.Console.WriteLine(produto.ImagemDoProdutoUrl);
-        System.Console.WriteLine(produto.CategoriaDoProduto.Descricao);
+        Palindromo(1000);
     }
-}
 
-public class Usuario
-{
-    public Guid Id { get; init; } = Guid.NewGuid();
-    public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string SenhaCriptografada { get; set; } = string.Empty;
-    public TipoDeUsuarioEnum? TipoDoUsuario { get; set; } = null;
-    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
-    public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-}
-
-public enum TipoDeUsuarioEnum
-{
-    vendedor,
-    comprador
-}
-
-public class Produto
-{
-    public Guid Id { get; init; } = Guid.NewGuid();
-    public Usuario? UsuarioCriador { get; init; } = null;
-    public string Nome { get; set; } = string.Empty;
-    public string Descricao { get; set; } = string.Empty;
-    public Idioma? IdiomaDoProduto { get; set; } = null;
-    public Pais? PaisDoProduto { get; set; } = null;
-    public string ImagemDoProdutoUrl { get; set; } = string.Empty;
-    public Categoria? CategoriaDoProduto { get; set; } = null;
-    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
-    public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-}
-
-public class Idioma
-{
-    public int Id { get; set; }
-    public string Descricao { get; set; } = string.Empty;
-    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
-    public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-}
-
-public class Pais
-{
-    public int Id { get; set; }
-    public string Descricao { get; set; } = string.Empty;
-    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
-    public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
-}
-
-public class Categoria
-{
-    public int Id { get; set; }
-    public string Descricao { get; set; } = string.Empty;
-    public DateTime? CriacaoDataHora { get; init; } = DateTime.UtcNow;
-    public DateTime? AlteracaoDataHora { get; set; } = DateTime.UtcNow;
 }
--- obj/Debug/net7.0/test.AssemblyInfo.cs
+++ obj/Debug/net7.0/test.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Treinamento-hot")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Treinamento-hot")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Treinamento-hot")]
+[assembly: System.Reflection.AssemblyProductAttribute("test")]
+[assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
